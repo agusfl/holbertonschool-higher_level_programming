648a8dcb0f8b803e914bc0941cd2fc7b999ef937
agusfl <agusflom@hotmail.com>
2022-04-25 05:50:12 -0700

    5 task with comments

---
+++ 0x00-python-hello_world/5-print_string_WITH_comments.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+str = "Holberton School"
+print("{}".format(3*str))
+print("{}".format(str[0:9]))
+# En python se puede hacer "slicing" que es indicar que partes se quiere imprimir, en este caso al poner 0:9 se
+# esta indicando que se imprima todo lo que este desde la posicion 0 hasta la 9.
