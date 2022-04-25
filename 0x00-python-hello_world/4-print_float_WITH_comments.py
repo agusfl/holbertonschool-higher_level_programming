a0e7ecad6acc95d03fb06a75c8a2701d7e4ce8ce
agusfl <agusflom@hotmail.com>
2022-04-25 14:23:41 -0700

    changing format for f

--- 0x00-python-hello_world/4-print_float_WITH_comments.py
+++ 0x00-python-hello_world/4-print_float_WITH_comments.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 number = 3.14159
-print("Float: {:3.2f}".format(number))
-# Con el 3 se le dice que se van a imprimir 3 numeros y con el .2f se le indica que es un numero de tipo float (con
+print(f"Float: {number:.2f}")
+
+# Con la f indicamos que vamos a imoprimir con formato (format) 
+# Con el .2f se le indica que es un numero de tipo float (con
 # coma) y se van a imprimir solo 2 digitos despues de la coma.
