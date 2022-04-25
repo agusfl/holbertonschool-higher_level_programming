3f824584cdf787d618fb53415947aff17213489f
agusfl <agusflom@hotmail.com>
2022-04-25 05:40:52 -0700

    4 task WITH comments

---
+++ 0x00-python-hello_world/4-print_float_WITH_comments.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+number = 3.14159
+print("Float: {:3.2f}".format(number))
+# Con el 3 se le dice que se van a imprimir 3 numeros y con el .2f se le indica que es un numero de tipo float (con
+# coma) y se van a imprimir solo 2 digitos despues de la coma.
