ac9df5a4828e875f23b0d8ac804af01c397b8071
agusfl <agusflom@hotmail.com>
2022-04-25 07:19:12 -0700

    9 task with comments

---
+++ 0x00-python-hello_world/9-easter_egg_EITH_comments.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import this
+
+# En este ejercicio nos piden que imprimamos el texto "The Zen of Python" by Tim Peters pero el script no puede tener
+# mas de 98 caracteres, y el texto (verlo en el eejercicio o buscarlo en internet) es mucho mas largo que 98
+# caracteres. Si se pone en el script: import this -> esto te importa el texto que nos piden que imprimamos.
