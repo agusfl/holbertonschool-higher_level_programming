84f3ae5c0c77ccd99b173f7f26d13b1162f830ea
agusfl <agusflom@hotmail.com>
2022-04-25 08:05:49 -0700

    more comments

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -7,21 +7,33 @@
  */
 int check_cycle(listint_t *list)
 {
+	/*
+	 * Se crea dos variables para recorrer la lista "list" una itera de a 2
+	 * y la otra de a 1
+	 */
 	listint_t *faster = list;
 	listint_t *slow = list;
 
-	if (!list) /* Si no existe la lista retornar 0*/
+	if (!list)
+	/*
+	 * Chequeamos si la lista que nos pasan existe y sino existe la lista se
+	 * retorna 0 y termina el prog
+	 */
 		return (0);
 
-	while (1) /* while infinito */
+	while (1) /* Se crea un while infinito */
 	{
 		/* Traverse los nodos mientras que la linked list exista */
 		if (faster->next != NULL && faster->next->next != NULL)
 		{
 			faster = faster->next->next; /* el mas rapido itera de a 2 */
-			slow = slow->next; /* itera de a 1 */
+			slow = slow->next; /* el mas lento itera de a 1 */
 
-			if (faster == slow) /* si son iguales retornar 1 ya que hay un ciclo */
+			if (faster == slow)
+			/*
+			 * si en algun momento son iguales retornar 1 ya que
+			 * hay un ciclo
+			 */
 				return (1);
 		}
 		else
