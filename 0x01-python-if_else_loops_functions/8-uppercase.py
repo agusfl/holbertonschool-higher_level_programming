a129effcf8b39a04c132a2c41d812450276cdaae
agusfl <agusflom@hotmail.com>
2022-04-26 08:55:32 -0700

    changinh f-string for .format for the checker

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,5 +3,5 @@ def uppercase(str):
     for i in str:
         if ord(i) >= 97 and ord(i) <= 123:
             i = chr(ord(i) - 32)
-        print(f"{i}", end='')
+        print("{}".format(i), end='')
     print()
