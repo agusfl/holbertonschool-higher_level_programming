819b27637c5cd7aefebebfe3a58cd2d0643f17f9
agusfl <agusflom@hotmail.com>
2022-04-26 06:26:50 -0700

    2 task and 2 task with comments

---
+++ 0x01-python-if_else_loops_functions/2-print_alphabet_WITH_comments.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for i in range (97, 123):
+    print(f"{chr(i)}", end='')
+# El abecedario en minuscula en ascii arranca en: 97 y termina en 122 (pongo 123 porque el range no agarra el ultimo
+# valor) dentro del formating (f) en el print casteo la variable 'i' a char poniendole: chr(i), por otro lado
+# le saco el salto de linea que tiene el print por default y pongo para que este vacio asi me lo imprime todo junto.
