95d32135aefc30ff0d99db92cd9ce6de7fe7d4dd
agusfl <agusflom@hotmail.com>
2022-04-26 08:46:24 -0700

    changing f-string for .format for the checker

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 for i in range(0, 10):
     for j in range((i+1), 10):
-        print(f"{i}{j}", end='')
+        print("{}{}".format(i,j), end='')
         if i is not 8 or j is not 9:
             print(end=', ')
         if i == 8 and j == 9:
