e1851936325ba266284f67eb0afc56b937e9d8f2
agusfl <agusflom@hotmail.com>
2022-04-26 06:39:09 -0700

    3 task and 3 task with comments

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt_WITH_comments.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+for i in range (97, 123):
+    if i == 101 or i == 113: # 101 es la letra 'e' en ascii y 113 la 'q', en caso que sea cualquiera de esas dos letras
+        # no quiero que las imprima por eso pongo un continue.
+        continue
+    else:
+        print(f"{chr(i)}", end='')
