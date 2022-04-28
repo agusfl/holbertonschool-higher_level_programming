4acfef58b2cc209c7f2640be209363a3e08f5598
agusfl <agusflom@hotmail.com>
2022-04-28 06:52:00 -0700

    2 task

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    if len(argv) == 1:
+        print(f"0 arguments.")
+    elif len(argv) == 2:
+        print(f"{len(argv) - 1} argument:")
+        print(f"{len(argv) - 1}: {argv[1]}")
+    elif len(argv) > 2:
+        print(f"{len(argv) - 1} arguments:")
+        for i in range(1, len(argv)):
+            print(f"{i}: {argv[i]}")
