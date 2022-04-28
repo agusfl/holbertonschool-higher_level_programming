82ddbb3370881f342fcc2a7eb98c66048d230014
agusfl <agusflom@hotmail.com>
2022-04-28 09:18:44 -0700

    more comments

--- 0x02-python-import_modules/100-my_calculator_WITH_comments.py
+++ 0x02-python-import_modules/100-my_calculator_WITH_comments.py
@@ -3,7 +3,10 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div # importamos el modulo que creamos como hicimos en los ejs anteriores
     from sys import argv # importamos el argv del modulo sys
     if (len(argv) - 1) != 3: # Ponemos -1 ya que te cuenta la posicion 0 que seria el ejecutable y como no queremos
-        # que se cuente le restamos 1.
+        # que se cuente le restamos 1. La condicion la ponemos si es distinto de 3 ya que se le tienen que pasar
+        # como comandos en linea a nuestro programa solo 3 argumentos, van a ser: el primer argumento va a ser el
+        # primer numero que queres operar luego el operador que queres usar siendo: +, -, *, / y el tercer argumento
+        # va a ser el tercer numero a operar.
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1) # Con esto le indicamos que salga con valor de 1 (para saber que fue un error) para ver en la consola
         # con que valor salio se usa: echo $? | en este caso hay que poner: ./100-my_calculator.py ; echo $? (con el
