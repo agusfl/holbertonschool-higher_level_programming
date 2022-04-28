c901cfd04939946b451beb26cd5f2fbd4d01c15a
agusfl <agusflom@hotmail.com>
2022-04-28 07:35:11 -0700

    4 task and 4 task with comments

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4
+    hidden = dir(hidden_4)
+    for i in hidden:
+        if (i[0] != '_' and i[1] != '_'):
+            print(i)
---
+++ 0x02-python-import_modules/4-hidden_discovery_WITH_comments.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import hidden_4 # Importamos hidden_4 ya que con el codigo de: curl ... que ponen en el ej 4 se importa el archivo desde
+    # github.
+    hidden = dir(hidden_4) # Hago una variable "hidden" donde guardo con la funcion dir() todo lo que haya en hidden_4
+    # y una vez que lo tenemos en la variable lo podemos iterar e ir imprimiendolo.
+    for i in hidden:
+        if (i[0] != '_' and i[1] != '_'): # Ponemos esta condicion ya que nos piden que no se impriman las variables con
+            # underscore (__) y esas variables arrancan con dos __ por lo tanto la posicion 0 y la 1 serian __ por eso
+            # ponemos que si es distinto se imprima 'i' que esta dentro del for que recorre lo que esta en hidden.
+            print(i)
