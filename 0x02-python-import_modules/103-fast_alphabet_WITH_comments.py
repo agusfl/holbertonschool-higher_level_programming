6392da8de318c7b9b7505d7e79aae7b584c636c9
agusfl <agusflom@hotmail.com>
2022-04-28 10:15:14 -0700

    9 task with comments

---
+++ 0x02-python-import_modules/103-fast_alphabet_WITH_comments.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+import string
+print(string.ascii_uppercase)
+
+# Usamos la libreria string que tiene una funcion para imprimir el abecedario en mayuscula usando ascii.
+# Dentro del print ponemos para que imprima la funcion.
