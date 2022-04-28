41cd2eb27667eb70b8290013c0ffcb5556e487b7
agusfl <agusflom@hotmail.com>
2022-04-28 09:55:14 -0700

    101 task with comments

---
+++ 0x02-python-import_modules/101-easy_print_WITH_comments.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hacking_print_101_task
+
+# Este ej es un poco engañoso ya que la tarea es bastante sencilla pero es un poco dificil darse cuenta de como
+# implementarla, basicamente nos piden que imprimamos en pantalla: #pythoniscool pero sin usar print ni eval ni open
+# ni importar sys (donde esta la funcion write) y el truco es hacer un modulo que en este caso le llame:
+# "hacking_print_101_task" en donde si uso el print con lo que nos piden y en este archivo solo llamo con el
+# import a ese modulo y ahi queda ya que en el archivo que hay que subir no se uso el print ni ninguno de los no
+# permitidos.
