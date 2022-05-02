708624b99e157787cc9f4a7f949b40fca1140d96
agusfl <agusflom@hotmail.com>
2022-05-02 08:24:41 -0700

    fixing border case

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -7,7 +7,7 @@ Function that replaces an element of a list at a specific position (like in C).
 def replace_in_list(my_list, idx, element):
     if idx < 0:
         return my_list
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return my_list
     else:
         my_list[idx] = element
