0726905297c9e7933c0d481bf2241c33f4735a27
agusfl <agusflom@hotmail.com>
2022-05-02 08:59:41 -0700

    fixing some comments

--- 0x03-python-data_structures/3-print_reversed_list_integer_WITH_comments.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer_WITH_comments.py
@@ -5,7 +5,12 @@ Function that prints all integers of a list, in reverse order.
 
 
 def print_reversed_list_integer(my_list=[]):
-    my_list.reverse() # Usamos el reverse() method para reversear la lista "my_list". Aca ya dejamos la lista
-    # reverseada y despues la recorremos con un for y la vamos imprimiendo.
-    for i in my_list:
-        print(f"{i}")
+    if my_list is not None:
+        my_list.reverse() # Usamos el reverse() method para reversear la lista "my_list". Aca ya dejamos la lista
+        # reverseada y despues la recorremos con un for y la vamos imprimiendo.
+        for i in my_list:
+            print(f"{i}")
+
+# Esta era una version de codigo que andaba pero el checker daba problemas, para solucionarlo tuve que usar
+# reversed en lugar de reverse asi: 
+# for i in reversed(my_list):
