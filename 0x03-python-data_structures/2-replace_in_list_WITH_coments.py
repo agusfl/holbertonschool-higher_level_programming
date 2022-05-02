58b80782dfc4a20a38ef2f682cd1d0da67624ea7
agusfl <agusflom@hotmail.com>
2022-05-02 06:17:22 -0700

    2 task with comments

---
+++ 0x03-python-data_structures/2-replace_in_list_WITH_coments.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+Function that replaces an element of a list at a specific position (like in C).
+"""
+
+
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    elif idx > len(my_list):
+        return my_list
+    else:
+        for i in my_list:
+            if i == idx:
+                my_list[idx] = element
+                return my_list
+
+# Es muy parecido al ejercicio 1, salvo que aca se quiere cambiar el elemento que este en la posicion especificada
+# por el valor que nos pasen en el tercer argumento de nuestra funcion "replace_in_list" que seria el argumento:
+# "element". Y cambian un poco los return de los casos "border".
