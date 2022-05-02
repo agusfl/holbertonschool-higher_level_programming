555c26fcdc3a5e6f9a67c37ab18c6f11db9064e4
agusfl <agusflom@hotmail.com>
2022-05-02 09:51:59 -0700

    7 task and 7 main in case you want to try the code

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Function that adds 2 tuples.
+"""
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    tuple_a += (0, 0)
+    tuple_b += (0, 0)
+    result1 = tuple_a[0] + tuple_b[0]
+    result2 = tuple_a[1] + tuple_b[1]
+    new_tuple = (result1, result2)
+    return new_tuple
