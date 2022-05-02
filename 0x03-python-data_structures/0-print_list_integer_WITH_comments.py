7fcd41cb76f4180e87cbb658a5b7965efd129e1d
agusfl <agusflom@hotmail.com>
2022-05-02 05:21:22 -0700

    0 task with comments

---
+++ 0x03-python-data_structures/0-print_list_integer_WITH_comments.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def print_list_integer(my_list=[]):
+    for i in my_list:
+        print(f"{i}")
+
+# Este ejercicio es sencillo, hay que recorrer la lista "my_list" e ir imprimiendo cada item de la lista que seria: 'i'
+# Recordar que en los ejercicios que nos pasan un archivo main como en este para ejecutar el codigo hay que ejecutar
+# el archivo main, ya que ahi llaman a nuestra funcion. 
+# Adicionalmente a partir de estos ejercicios nos habilitaron otra herramienta que esta muy buena, se llama: Trace
+# es una pagina que sirve para probar el codigo y te muestra paso a paso como se va ejecutando el mismo.
