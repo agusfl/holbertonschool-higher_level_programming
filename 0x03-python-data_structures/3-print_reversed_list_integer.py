2d28948bc56981defe4c43d5228637e149446728
agusfl <agusflom@hotmail.com>
2022-05-02 08:42:38 -0700

    fix checker

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,5 +8,6 @@ def print_reversed_list_integer(my_list=[]):
     my_list.reverse()
     if my_list is None:
         return
-    for i in my_list:
-        print(f"{i:d}")
+    else:
+        for i in my_list:
+            print(f"{i:d}")
