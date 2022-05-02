09e91f2f1a9c303ba0bee785c53d37c87d6b6218
agusfl <agusflom@hotmail.com>
2022-05-02 07:50:48 -0700

    6 task and 6 main in case you want to try the code

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that prints a matrix of integers.
+"""
+
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            if i is not row[len(row) - 1]:
+                print(f"{i}", end= " ")
+            else:
+                print(f"{i}", end="")
+        print()
