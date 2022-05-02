51e8316355cd0ff26d6daad64a6f20b07c5b20cb
agusfl <agusflom@hotmail.com>
2022-05-02 07:01:07 -0700

    4 task and 4 main in case you want to try the code

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Write a function that replaces an element in a list at a specific position without modifying the original list.
+"""
+
+
+def new_in_list(my_list, idx, element):
+    copy_list = my_list.copy()
+    if idx < 0 or idx > len(copy_list):
+        return copy_list
+    else:
+        copy_list[idx] = element
+        return copy_list
