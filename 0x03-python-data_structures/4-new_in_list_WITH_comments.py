7b329969ed2d8a48e2b74dbecc5c20caa9b7eee6
agusfl <agusflom@hotmail.com>
2022-05-02 09:03:51 -0700

    fix

--- 0x03-python-data_structures/4-new_in_list_WITH_comments.py
+++ 0x03-python-data_structures/4-new_in_list_WITH_comments.py
@@ -6,7 +6,7 @@ Write a function that replaces an element in a list at a specific position witho
 
 def new_in_list(my_list, idx, element):
     copy_list = my_list.copy()
-    if idx < 0 or idx > len(copy_list):
+    if idx < 0 or idx >= len(copy_list):
         return copy_list
     else:
         copy_list[idx] = element
