1a7c85d49e993e9d17addf21054c36327bd4916b
agusfl <agusflom@hotmail.com>
2022-05-02 08:30:54 -0700

    fixing border case

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -7,7 +7,7 @@ Function that retrieves an element from a list line in C.
 def element_at(my_list, idx):
     if idx < 0:
         return
-    elif idx > len(my_list):
+    elif idx >= len(my_list):
         return
     else:
         for i in my_list:
