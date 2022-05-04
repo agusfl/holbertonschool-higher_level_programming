7a6ce3ed225f28ac3078592165980397c3a6ccaa
agusfl <agusflom@hotmail.com>
2022-05-04 08:07:10 -0700

    fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,11 +1,8 @@
 #!/usr/bin/pyton3
 """
 Function that returns a set of common elements in two sets.
-Uso la funcion "intersection" para sacar los datos iguales de ambos sets.
-En este ej tmb se podria usar el operador & de la
-siguiente manera --> return set_1 & set_2
 """
 
 
 def common_elements(set_1, set_2):
-    return set_1.intersection(set_2)
+    return set_1 & set_2
