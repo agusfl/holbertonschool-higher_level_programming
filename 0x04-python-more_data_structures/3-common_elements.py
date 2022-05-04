6ff4d751f99e46a497e0662fe16a04508513d504
agusfl <agusflom@hotmail.com>
2022-05-04 08:11:10 -0700

    fix

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,8 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
 """
 Function that returns a set of common elements in two sets.
+Tambien se puede usar el metodo "intersection", de la siguiente forma:
+return set_1.intersection(set_2)
 """
 
 
