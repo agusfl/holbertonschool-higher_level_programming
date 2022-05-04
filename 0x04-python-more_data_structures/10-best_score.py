796bfaf3a0e86ee35b9d1c0a7a2e0fee988b2141
agusfl <agusflom@hotmail.com>
2022-05-04 09:35:38 -0700

    10 task

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Function that returns a key with the biggest integer value.
+"""
+
+
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    else:
+        return max(a_dictionary, key=a_dictionary.get)
