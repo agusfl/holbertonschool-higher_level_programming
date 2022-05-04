899d3755c2de21c9e12f0070b20a42457cbace2f
agusfl <agusflom@hotmail.com>
2022-05-04 07:01:33 -0700

    3 task with some comments

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyton3
+"""
+Function that returns a set of common elements in two sets.
+Uso la funcion "intersection" para sacar los datos iguales de ambos sets.
+"""
+
+
+def common_elements(set_1, set_2):
+    return set_1.intersection(set_2)
