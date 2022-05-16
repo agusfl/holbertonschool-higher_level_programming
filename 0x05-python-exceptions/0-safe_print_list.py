65e6b67cb385f1d49da93ec25e0bf35ef5f1c781
agusfl <agusflom@hotmail.com>
2022-05-16 06:28:22 -0700

    0 task and 0 main in case you want to try the code

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that prints x elements of a list.
+"""
+
+
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while i < x:
+        try:
+            print (f"{my_list[i]}", end="")
+        except:
+            break
+        i += 1
+    print ("")
+    return i
