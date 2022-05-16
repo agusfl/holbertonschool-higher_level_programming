3ac5f096b5d0dcc913706f5d14be2c63bd314002
agusfl <agusflom@hotmail.com>
2022-05-16 06:51:02 -0700

    add name exception - fix pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -8,9 +8,9 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            print (f"{my_list[i]}", end="")
-        except:
+            print(f"{my_list[i]}", end="")
+        except IndexError:
             break
         i += 1
-    print ("")
+    print("")
     return i
