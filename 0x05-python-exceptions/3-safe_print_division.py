c8610acdf43a1eb618734c05d3b1aea4a3ab003e
agusfl <agusflom@hotmail.com>
2022-05-16 08:57:52 -0700

    3 main 3 task

---
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+Function that divides 2 integers and prints the result.
+"""
+
+
+def safe_print_division(a, b):
+    try:
+        result = a / b
+    except ZeroDivisionError:
+        result = None
+    finally:
+        print("Inside result: {}".format(result))
+    return result
