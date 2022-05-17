9a0d2e4aff79fd1740b75474639efcbeefd9b4f4
agusfl <agusflom@hotmail.com>
2022-05-17 16:03:57 +0000

    more comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -10,5 +10,11 @@ class Square:
         """
         Initialize the square, attributes:
             size --> it is the size of the square.
+        Reason for size to bue a private attribute (__size):
+        The size of a square is crucial for a square, many things depend of it
+        (area computation, etc.), so you, as class builder, must control the
+        type and value of this attribute. One way to have the control is to
+        keep it privately. You will see in next tasks how to get, update and
+        validate the size value.
         """
         self.__size = size
