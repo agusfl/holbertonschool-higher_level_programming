b765e6dff282aae2b279a471d94d8c7dc40af46b
agusfl <agusflom@hotmail.com>
2022-05-17 15:45:26 +0000

    2 task and 2 main in case you want to try the code

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,19 @@
+#!/bin/usr/python3
+"""
+Class square that defines a square.
+"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size=0):
+        """
+        Initialize the square, attributes:
+            size (private attribute) --> it is the size of the square.
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
