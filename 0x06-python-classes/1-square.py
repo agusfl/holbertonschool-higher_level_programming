106de743ae349ee574cbe689e39e4b391bd84bfc
agusfl <agusflom@hotmail.com>
2022-05-17 15:03:50 +0000

    1 task and 1 main in case you want to try the code

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+"""
+Class square that defines a square.
+"""
+
+
+class Square:
+    """Class Square"""
+    def __init__(self, size):
+        """
+        Initialize the square, attributes:
+            size --> it is the size of the square.
+        """
+        self.__size = size
