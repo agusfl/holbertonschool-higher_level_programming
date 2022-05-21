1c33778b819fd7b569cd77b40560628e471b6627
agusfl <agusflom@hotmail.com>
2022-05-21 00:19:24 +0000

    some changes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""
+Module 2-matrix_divided, this module has a function that divides all
+elements of a matrix.
+"""
+
+
+def matrix_divided(matrix, div):
+    """
+    Function that divides all elements of a matrix.
+    """
+
