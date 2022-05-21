40d0539772a34a9468b595fe36d1f4150dc1ca57
agusfl <agusflom@hotmail.com>
2022-05-21 16:22:36 +0000

    function with comments

--- 0x07-python-test_driven_development/2-matrix_divided_WITH_comments.py
+++ 0x07-python-test_driven_development/2-matrix_divided_WITH_comments.py
@@ -10,6 +10,8 @@ def matrix_divided(matrix, div):
     Function that divides all elements of a matrix.
     """
 
+    # Analizamos los casos de letra, si el tipo de dato que se pasa no es una list y si los elementos 
+    # de las listas que conforman la matrix no son integers o floats.
     if type(matrix) is not list or len(matrix) == 0:
         raise TypeError("matrix must be a matrix (list of lists) of \
                 integers/floats")
@@ -17,20 +19,26 @@ def matrix_divided(matrix, div):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
-    new_matrix = []
+
+    new_matrix = [] 
+    # Se crea una matrix vacia para completarla con el resultado de dividir cada elemento de la matrix
+    # entre el numero que se pase en: div
     for lists in matrix:
+        # Se hace un for que recorra todas las listas que esten dentro de la matrix y en caso que las listas no sean
+        # de tipo "list" se levanta (raise) un mensaje de error 
         if type(lists) is not list:
             raise TypeError("matrix must be a matrix (list of lists) of \
                 integers/floats")
             if len(lists) != len(matrix[0]):
-            """
-            len matrix[0] es el largo de la primer lista de la matrix, seria 3
-            en el caso del main de prueba.
-            """
+    # len matrix[0] es el largo de la primer lista de la matrix, seria 3
+    # en el caso del main de prueba.
             raise TypeError("Each row of the matrix must have the same size")
-        new_list = []
-        for position in lists:
+        new_list = [] # Creamos una lista vacia donde vamos a ir guardando con el metodo append el nuevo resultado
+        # que va a ir en la lista, osea el elemento dividido el numero que se pase en "div".
+        for position in lists: # "position" seria cada elemento y lo vamos a dividir por "div".
             if type(position) is not int and type(position) is not float:
+            # Si el tipo de position no es int o float hacemos un raise de TypeError con el mensaje que nos dicen
+            # en la letra.
                 raise TypeError("matrix must be a matrix (list of lists) of \
                 integers/floats")
             new_list.append(round(position/div, 2))
