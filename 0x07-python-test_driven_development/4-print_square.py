77e4c576ba2ea0d74aa277801ab558b5b310e9aa
agusfl <agusflom@hotmail.com>
2022-05-21 21:31:13 +0000

    function for printing a square with #

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,1 +1,20 @@
 #!/usr/bin/python3
+"""
+Module 4-print_square, this module has a function that prints a square
+with the character: #
+"""
+
+
+def print_square(size):
+    """
+    Function that prints a square with the character: #
+    """
+    if type(size) is not int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    else:
+        for row in range(size):
+            for column in range(size):
+                print("#", end="")
+            print()
