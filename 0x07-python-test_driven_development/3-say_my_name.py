a1fff693bef909f3cabee085b7e601a7154a3b6e
agusfl <agusflom@hotmail.com>
2022-05-21 00:19:13 +0000

    function implementation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+"""
+Module: 3-say_my_name defines a function that print a phrase.
+"""
+
+
+def say_my_name(first_name, last_name=""):
+    """
+    Function that print first and last name passed as arguments.
+    """
+    if type(first_name) is not str:
+        raise TypeError("first_name must be a string")
+    elif type(last_name) is not str:
+        raise TypeError("last_name must be a string")
+    else:
+        print("My name is {} {}".format(first_name, last_name))
