7947a10758c951e9e7c45d8f05227587c69f1345
agusfl <agusflom@hotmail.com>
2022-05-24 03:11:34 +0000

    101 function

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,1 +1,14 @@
 #!/usr/bin/python3
+import numpy as np
+"""
+Module 101-lazy_matrix_mul
+This is a module for multiply two matrices using numpy module
+To install numpy on ubuntu use: pip install numpy
+"""
+
+
+def lazy_matrix_mul(m_a, m_b):
+    """
+    Using numpy module to multiply 2 matrices.
+    """
+    return np.dot(m_a, m_b)
