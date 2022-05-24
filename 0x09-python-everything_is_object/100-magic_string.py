701738c520400605339712f5989cedaaa8aba38b
agusfl <agusflom@hotmail.com>
2022-05-24 16:14:40 +0000

    changing list name

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def magic_string(string=[]):
-    string += ["BestSchool"]
-    return ', '.join(string)
+def magic_string(string_list=[]):
+    string_list += ["BestSchool"]
+    return ', '.join(string_list)
