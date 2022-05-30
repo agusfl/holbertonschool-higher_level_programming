0774f25164ea62b1053cfbe8f49420b981efbf91
agusfl <agusflom@hotmail.com>
2022-05-30 21:15:20 +0000

    change import

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """
 Create a class Square that inherits from class: Rectangle.
 """
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
