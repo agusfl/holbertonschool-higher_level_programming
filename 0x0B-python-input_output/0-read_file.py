a0872c045c0b35f702db06afd84481f465203391
agusfl <agusflom@hotmail.com>
2022-05-31 12:48:05 +0000

    0 task, 0 main and my_file txt with the text to read in case you want to try the code

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Function that reads a text file (with UTF8 encoding) and prints it to stdout
+"""
+
+
+def read_file(filename=""):
+    """
+    Function to read a file and print it to stdout
+    """
+    with open(filename, encoding="utf-8") as f:
+        read_data = f.read()
+        print(read_data)
