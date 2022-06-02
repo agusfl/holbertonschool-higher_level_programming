b3fbaa6f31641b5b4e9b0f4beb31e0ae3798956c
agusfl <agusflom@hotmail.com>
2022-06-02 21:36:28 +0000

    rectangle.py with some comments

---
+++ 0x0C-python-almost_a_circle/models/rectangle_WITH_comments.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python3
+"""
+Create a class Rectangle that inherits from class Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter func for private attribute: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set value for: width
+        """
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter func for private attribute: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set value for: height
+        """
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter func for private attribute: x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set value for: x
+        """
+        if type(value) is not int:
+            raise TypeError("x must be an integer")
+        if value < 0:
+            raise ValueError("x must be >= 0")
+        else:
+            self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter func for private attribute: y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set value for: y
+        """
+        if type(value) is not int:
+            raise TypeError("y must be an integer")
+        if value < 0:
+            raise ValueError("y must be >= 0")
+        else:
+            self.__y = value
+
+    def area(self):
+        """
+        Public instance method that returns the rectangle area
+        """
+        return self.__width * self.__height
+
+    def display(self):
+        """
+        Public method that prints in stdout the Rectangle instance
+        with the character: #
+        """
+        for break_line in range(self.__y):
+            print()
+        for row in range(self.__height):
+            for blank in range(self.__x):
+                print(" ", end="")
+            for column in range(self.__width):
+                print("#",  end="")
+            print()
+
+    def __str__(self):
+        """
+        Prints the Rectangle, we use the built in function
+        __str__ - we will print with the format:
+        [Rectangle] (<id>) <x>/<y> - <width>/<height>
+        """
+        return "[{}] ({}) {}/{} - {}/{}".format(
+            self.__class__.__name__, self.id, self.__x, self.__y,
+             self.__width, self.__height)
+
+    def update(self, *args):
+        """
+        Public method that assigns an argument to each attribute:
+        1st argument should be the id attribute
+        2nd argument should be the width attribute
+        3rd argument should be the height attribute
+        4th argument should be the x attribute
+        5th argument should be the y attribute
+        """
+        attrs = ["id", "width", "height", "x", "y"]
+
+        for position, arg in enumerate(args):
+            # Usamos el metodo enumerate para poder hacer el "unpack".
+            if position > (len(attrs) - 1): 
+                # Si la posicion en la que se esta en el loop es mayor al largo
+                # de la lista attrs - 1 osea: 4 quiero que haga un break ya que
+                # no tenemos ningun atributo para asignar a lo que sea que 
+                # pasen.
+                break
+            else:
+                setattr(self, attrs[position], arg)
+                # Usamos el metodo setattr para setear el objeto que pasen (hacemos referencia a esto con "self")
+                # Como segundo argumento le pasamos lo que este en la lista "attr" en la posicion que estemos recorriendo
+                # dentro del for. 
+                # Como ultimo argumento le pasamos "arg" que es lo que vamos a asignar, esto lo sacamos de la iteracion
+                # dentro del for que itera lo que este en "args" osea los argumentos variables que se pasen dentro del metodo
+                # que estamos creando ("update").
