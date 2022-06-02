82fb42eae2bdf237bf7146556d968306000d77fc
agusfl <agusflom@hotmail.com>
2022-06-02 16:54:51 +0000

    2 task and main in case you want to try the code

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+Create a class Rectangle that inherits from class Base
+"""
+from models.base import Base
+
+
+class Rectangle(Base):
+    """class Rectangle inherits from Base"""
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Initialization"""
+        super().__init__(id)
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+    @property
+    def width(self):
+        """
+        Getter func for private attribute: width
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """
+        Set value for: width
+        """
+        self.__width = value
+
+    @property
+    def height(self):
+        """
+        Getter func for private attribute: height
+        """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """
+        Set value for: height
+        """
+        self.__height = value
+
+    @property
+    def x(self):
+        """
+        Getter func for private attribute: x
+        """
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """
+        Set value for: x
+        """
+        self.__x = value
+
+    @property
+    def y(self):
+        """
+        Getter func for private attribute: y
+        """
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """
+        Set value for: y
+        """
+        self.__y = value
