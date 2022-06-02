f41e95f2abad1f525e2559f3098ba54275b1157f
agusfl <agusflom@hotmail.com>
2022-06-02 15:54:23 +0000

    fix - removing type()

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -14,7 +14,7 @@ class Base:
         This class will be the “base” of all other classes in this project.
         The goal of it is to manage id attribute in all your future classes and
         to avoid duplicating the same code (by extension, same bugs)"""
-        if type(id) is not None:
+        if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
