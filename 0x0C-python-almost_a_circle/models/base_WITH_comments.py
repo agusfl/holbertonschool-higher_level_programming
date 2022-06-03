49af764f385fe66406f42e9a8d5504849e8f4688
agusfl <agusflom@hotmail.com>
2022-06-03 18:18:10 +0000

    comments - task 15

---
+++ 0x0C-python-almost_a_circle/models/base_WITH_comments.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+Module for creating a class: Base
+"""
+
+
+class Base:
+    """class Base"""
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """Initialize id, increment class attribute __nb_objects if there is
+        no id and set it as id.
+        This class will be the “base” of all other classes in this project.
+        The goal of it is to manage id attribute in all your future classes and
+        to avoid duplicating the same code (by extension, same bugs)"""
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """
+        JSON is one of the standard formats for sharing data representation.
+        Update the class Base by adding the static method
+        def to_json_string(list_dictionaries -> dictionary): that returns the
+        JSON string representation of list_dictionaries static method es un
+        metodo que se puede usar por fuera de la clase, es parecido a definir
+        una funcion normal.
+        """
+        import json
+        if list_dictionaries is None or len(list_dictionaries) > 0:
+            return json.dumps(list_dictionaries)
+        else:
+            json.dump(list_dictionaries)
