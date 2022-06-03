ce660e45c7f50ce38e8d47523534ba81de3d291a
agusfl <agusflom@hotmail.com>
2022-06-03 14:53:09 +0000

    comments - for 11 task

--- 0x0C-python-almost_a_circle/models/square_WITH_comments.py
+++ 0x0C-python-almost_a_circle/models/square_WITH_comments.py
@@ -32,3 +32,30 @@ class Square(Rectangle):
 # Es lo mismo ponerle height o width para el size ya que al ser un cuadrado tanto el height como el width
 # van a medir lo mismo.
 # Aca para llamar a los atributos se llama de manera publica (con el punto) y no con el doble underscore porque usamos el super().
+    
+    @property
+    def size(self):
+        """
+        Getter func for private attribute: size
+        """
+        return self.width # aca es lo mismo si ponemos width o height ya que van a tener el mismo valor (porque es un cuadrado).
+    
+    @size.setter
+    def size (self, value):
+        """
+        Set value for: size
+        The setter should assign (in this order) the width and the height
+        with the same value.
+        The setter should have the same value validation as the Rectangle for
+        width and height - No need to change the exception error message (It
+        should be the one from width).
+        """
+        self.width = value
+        self.height = value
+    # Aca lo que se hace es agregar un getter (con el decorador property) y un setter (con el decorador)
+    # para el atrbiuto "size".
+    # Como nos dicen que el mensaje de error por si el tipo de dato que pasan no es un int y demas tiene que ser
+    # el mismo que el de la clase padre, dejamos todo como esta y lo unico que hacemos es definir tanto width como
+    # height iguales al valor que se pase (value) ya que como es un cuadrado van a tener el mismo "value" y en caso de
+    # error nos va a traer el mensaje que se establecio en la clase padre para width y height.
+ 
