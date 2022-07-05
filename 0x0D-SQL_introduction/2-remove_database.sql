f50708944cfe30221618713008350c754fc2ebbf
agusfl <agusflom@hotmail.com>
2022-07-05 15:19:22 +0000

    task 2

---
+++ 0x0D-SQL_introduction/2-remove_database.sql
@@ -0,0 +1,5 @@
+-- SQL script that deletes the database hbtn_0c_0 in my MySQL server.
+-- If the database hbtn_0c_0 doesn’t exist, your script should not fail
+-- Info: https://linuxize.com/post/how-to-delete-a-mysql-database/
+
+DROP DATABASE IF EXISTS hbtn_0c_0;
