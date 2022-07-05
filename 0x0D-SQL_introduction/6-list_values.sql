0d062221baaab1485ec80ad01b0c1634f13c0e35
agusfl <agusflom@hotmail.com>
2022-07-05 16:47:10 +0000

    task 6

--- 0x0D-SQL_introduction/6-list_values.sql
+++ 0x0D-SQL_introduction/6-list_values.sql
@@ -1,5 +1,5 @@
 -- Script that lists all rows of the table "first_table" from the database hbtn_0c_0 in my MySQL server.
 -- The database name will be passed as an argument of the mysql command
--- Info:
+-- Info: http://g2pc1.bu.edu/~qzpeng/manual/MySQL%20Commands.htm
 
 SELECT * FROM first_table;
