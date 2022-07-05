2bb9e3033640bdc0ade4e70184d29cf1587fb8d0
agusfl <agusflom@hotmail.com>
2022-07-05 19:03:04 +0000

    task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -2,4 +2,4 @@
 -- The list should be sorted by the number of records (descending)
 -- The database name will be passed as an argument to the mysql command
 
-SELECT score, COUNT(score) as number FROM second_table GROUP BY score;
+SELECT score, COUNT(score) as number FROM second_table GROUP BY score ORDER BY score DESC;
