0107dcdd49b55d44e913e7686368d210dc5b292b
agusfl <agusflom@hotmail.com>
2022-07-05 21:29:29 +0000

    adding alias

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,4 +1,4 @@
 -- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0)
 -- Script that displays the max temperature of each state (ordered by State name).
 
-SELECT state, MAX(value) FROM temperatures GROUP BY state ORDER BY state;
+SELECT state, MAX(value) AS mas_temp FROM temperatures GROUP BY state ORDER BY state;
