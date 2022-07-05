042520d2c482fbfdb200738868345113e93b36eb
agusfl <agusflom@hotmail.com>
2022-07-05 15:24:09 +0000

    adding comments

--- 0x0D-SQL_introduction/1-create_database_if_missing.sql
+++ 0x0D-SQL_introduction/1-create_database_if_missing.sql
@@ -1,4 +1,5 @@
 -- Script that creates the database: hbtn_0c_0 in my MySQL server.
+-- If the database hbtn_0c_0 already exists, your script should not fail
 -- Info: https://linuxize.com/post/how-to-create-a-mysql-database/
 
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
