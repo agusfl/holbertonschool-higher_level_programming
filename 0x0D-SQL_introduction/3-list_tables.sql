865c60fcef4a8b62d78f0eaacec5a45974bc3e96
agusfl <agusflom@hotmail.com>
2022-07-05 15:58:04 +0000

    task 3

--- 0x0D-SQL_introduction/3-list_tables.sql
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -2,4 +2,4 @@
 -- The database name will be passed as argument of mysql command
 -- Info: https://linuxize.com/post/show-tables-in-mysql-database/#:~:text=To%20get%20a%20list%20of,run%20the%20SHOW%20TABLES%20command.&text=The%20optional%20FULL%20modifier%20will,as%20a%20second%20output%20column.
 
-SHOW TABLES LIKE '%';
+SHOW TABLES;
