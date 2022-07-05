1b4ac494f049e457b90491cf42a407acd1651039
agusfl <agusflom@hotmail.com>
2022-07-05 16:37:01 +0000

    task 5

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,6 @@
+-- Script that prints the full description of the table "first_table" from the database "hbtn_0c_0" in my MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- You are not allowed to use the DESCRIBE or EXPLAIN statements
+-- Info: https://stackoverflow.com/questions/1498777/how-do-i-show-the-schema-of-a-table-in-a-mysql-database 
+
+SHOW CREATE TABLE first_table;
