0a38947fda4fb6d9ab84159eb653ace4934edfbb
agusfl <agusflom@hotmail.com>
2022-07-05 16:55:01 +0000

    task 7

---
+++ 0x0D-SQL_introduction/7-insert_value.sql
@@ -0,0 +1,5 @@
+-- Script that inserts a new row in the table first_table (database hbtn_0c_0) in my MySQL server.
+-- The database name will be passed as an argument of the mysql command
+-- Info: https://www.guru99.com/insert-into.html
+
+INSERT INTO first_table (id,name) VALUES (89,'Best School');
