e5973468496906544b424a5df4b6dc1cce07547d
agusfl <agusflom@hotmail.com>
2022-07-05 17:13:39 +0000

    task 8

--- 0x0D-SQL_introduction/8-count_89.sql
+++ 0x0D-SQL_introduction/8-count_89.sql
@@ -1,5 +1,5 @@
 -- Script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in my MySQL server.
 -- The database name will be passed as an argument of the mysql command
--- Info: https://www.tutorialspoint.com/how-to-count-the-number-of-tables-in-a-mysql-database#:~:text=To%20check%20the%20count%20of,count%20of%20all%20the%20tables. 
+-- Info COUNT() function: https://www.javatpoint.com/mysql-count#:~:text=MySQL%20count()%20function%20is,not%20find%20any%20matching%20rows.
 
 SELECT COUNT(*) FROM first_table WHERE id=89;
