4e4525dfc91cac7750d3c28d923668a4c1217427
agusfl <agusflom@hotmail.com>
2022-07-05 15:44:49 +0000

    task 3

---
+++ 0x0D-SQL_introduction/3-list_tables.sql
@@ -0,0 +1,5 @@
+-- Script that lists all the tables of a database in my MySQL server.
+-- The database name will be passed as argument of mysql command
+-- Info: https://linuxize.com/post/show-tables-in-mysql-database/#:~:text=To%20get%20a%20list%20of,run%20the%20SHOW%20TABLES%20command.&text=The%20optional%20FULL%20modifier%20will,as%20a%20second%20output%20column.
+
+SHOW TABLES LIKE '%';
