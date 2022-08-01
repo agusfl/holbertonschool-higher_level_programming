99dab276006acdc6508ad0d0355081e25d25d78d
agusfl <agusflom@hotmail.com>
2022-08-01 18:49:48 +0000

    fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,5 +1,6 @@
 #!/usr/bin/node
 // Script that searches the second biggest integer in the list of arguments.
+// metodo push --> https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/Array/push
 
 const process = require('process'); // agrego el modulo process usando "require" (se podria usar import tmb)
 const args = process.argv; // defino args como process.argv asi uso args en el resto del codigo.
@@ -12,7 +13,7 @@ if (!args[2]) {
 } else {
   for (let i = 2; i < args.length; i++) {
     array.push(parseInt(args[i]));
-    array.sort();
+    array.sort(function (a, b) { return a - b; });
     array = [...new Set(array)];
   }
   console.log(array[array.length - 2]);
