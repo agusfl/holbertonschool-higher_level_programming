7c37f4f297946a0a49bdabdb0a4b9d07446adc36
agusfl <agusflom@hotmail.com>
2022-08-01 18:31:29 +0000

    task 11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+// Script that searches the second biggest integer in the list of arguments.
+
+const process = require('process'); // agrego el modulo process usando "require" (se podria usar import tmb)
+const args = process.argv; // defino args como process.argv asi uso args en el resto del codigo.
+const array = [];
+
+if (!args[2]) {
+  console.log('0');
+} else if (args.length === 3) {
+  console.log('0');
+} else {
+  for (let i = 2; i < args.length; i++) {
+    array.push(parseInt(args[i]));
+    array.sort();
+  }
+  console.log(array[array.length - 2]);
+}
