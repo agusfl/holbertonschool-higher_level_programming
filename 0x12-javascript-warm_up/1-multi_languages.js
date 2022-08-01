8339cf7342d725af311b12ac3355116cdb1a07e4
agusfl <agusflom@hotmail.com>
2022-08-01 13:43:36 +0000

    task 1

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Script for printing 3 messages
+const var1 = 'C is fun';
+const var2 = 'Python is cool';
+const var3 = 'JavaScript is amazing';
+console.log(var1);
+console.log(var2);
+console.log(var3);
