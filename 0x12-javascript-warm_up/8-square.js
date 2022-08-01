c127892c6f9f794f58bc04f60a9cc6d2598beb96
agusfl <agusflom@hotmail.com>
2022-08-01 16:55:21 +0000

    task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+// Script that prints a square
+// repeat method --> https://developer.mozilla.org/es/docs/Web/JavaScript/Reference/Global_Objects/String/repeat
+
+const process = require('process'); // agrego el modulo process usando "require" (se podria usar import tmb)
+const args = process.argv; // defino args como process.argv asi uso args en el resto del codigo.
+const size = args[2];
+
+if (!size) {
+  console.log('Missing size');
+} else {
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
+  }
+}
