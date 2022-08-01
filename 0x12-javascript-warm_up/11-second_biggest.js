57709c92f0bf836c92b17810d30bec139d590b03
agusfl <agusflom@hotmail.com>
2022-08-01 18:39:56 +0000

    fix repetead numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,7 +3,7 @@
 
 const process = require('process'); // agrego el modulo process usando "require" (se podria usar import tmb)
 const args = process.argv; // defino args como process.argv asi uso args en el resto del codigo.
-const array = [];
+let array = [];
 
 if (!args[2]) {
   console.log('0');
@@ -13,6 +13,7 @@ if (!args[2]) {
   for (let i = 2; i < args.length; i++) {
     array.push(parseInt(args[i]));
     array.sort();
+	array = [...new Set(array)];
   }
   console.log(array[array.length - 2]);
 }
