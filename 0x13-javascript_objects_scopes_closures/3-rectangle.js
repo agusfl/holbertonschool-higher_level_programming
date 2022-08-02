04fe19f87940fb08e6719660011d37087662680e
agusfl <agusflom@hotmail.com>
2022-08-02 15:17:39 +0000

    task 3

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+// Create an instance method called print() that prints the rectangle using the character X
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w <= 0 || h <= 0 || w === undefined || h === undefined) {
+      return;
+    }
+    this.width = w;
+    this.height = h;
+  }
+
+  print () {
+    const rows = this.height;
+    const columns = this.width;
+    for (let i = 0; i < rows; i++) {
+      console.log('X'.repeat(columns));
+    }
+  }
+};
