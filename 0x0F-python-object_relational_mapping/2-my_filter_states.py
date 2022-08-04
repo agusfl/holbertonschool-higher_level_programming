284e56b3e47ce5b5d25c0a051e7210fc43c399f4
agusfl <agusflom@hotmail.com>
2022-08-04 15:10:49 +0000

    task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -39,6 +39,8 @@ if __name__ == "__main__":
         # poder hacer consultas de SQL
         cur = conn.cursor()
         # Escibimos la SQL query usando la funcion "execute":
+        # If you want to spread strings over multiple lines, you also have the
+        # option of escaping a return with a \:
         cur.execute(f"SELECT *FROM states WHERE name = '{stateName_search}'\
                     ORDER BY id ASC")
         # Se guardan los resultados de la query que ejecutamos con "execute"
