932f1eeb57e2c72b5c6f946020e418f71e8e3313
agusfl <agusflom@hotmail.com>
2022-08-05 02:15:24 +0000

    task 11

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Write a script that adds the State object “Louisiana” to the database
+hbtn_0e_6_usa:
+
+- Your script should take 3 arguments: mysql username, mysql password and
+database name
+- You must use the module SQLAlchemy
+- You must import State and Base from model_state - from model_state import
+Base, State
+- Your script should connect to a MySQL server running on localhost at port3306
+- Print the new states.id after creation
+- Your code should not be executed when imported
+
+How to add data to a database using sqlalchemy in python:
+https://www.google.com/search?q=how+to+add+data+to+a+database+in+sqlalchemy+
+python&oq=how+to+add+data+to+a+database+in+sqlalc&aqs=chrome.2.69i57j33i10i160
+l2.11079j0j7&sourceid=chrome&ie=UTF-8#kpvalbx=_03rsYpvADcmO4dUPgrKZkA816
+"""
+import sys
+# importamos lo que creamos en el script model_state.py
+from model_state import Base, State
+# Import SQLAlchemy
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import sessionmaker
+
+
+if __name__ == "__main__":
+    # Connect to MySQL server running on localhost at port 3306
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
+
+    # Uso la funcion Session para abrir la sesion
+    Session = sessionmaker(bind=engine)  # se une la sesion con el engine
+    session = Session()  # creamos una instancia de "Session"
+
+    # Create object to add to the database --> hbtn_0e_6_usa
+    Louisiana = State(id=6, name="Louisiana")
+    # Print id after creation of the object:
+    print(Louisiana.id)
+
+    # Add object to table "states"
+    session.add(Louisiana)
+    # Save new info in table
+    session.commit()
+
+    # Close session that was opened
+    session.close()
