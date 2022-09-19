9f4a2d4717621c68ec5c0ba7ddc66a4927c226e0
agusfl <agusflom@hotmail.com>
2022-09-19 14:14:35 +0000

    modify comments

--- 0x15-javascript-web_jquery/2-script.js
+++ 0x15-javascript-web_jquery/2-script.js
@@ -1,4 +1,5 @@
-// Indo JQuery: https://stackoverflow.com/questions/2941679/change-text-color-using-jquery
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000) when the user clicks on the tag DIV#red_header
+// Info JQuery: https://stackoverflow.com/questions/2941679/change-text-color-using-jquery
 // Defino $ para que no haya problemas con el semistandard
 const $ = window.$;
 
