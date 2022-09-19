aebb16b2dc6dd019e18e420a4ac8b38f0028f8c4
agusfl <agusflom@hotmail.com>
2022-09-19 13:49:43 +0000

    task 1 - with some comments

---
+++ 0x15-javascript-web_jquery/1-script.js
@@ -0,0 +1,5 @@
+// Indo JQuery: https://www.w3schools.com/jquery/jquery_syntax.asp
+// Defino $ para que no haya problemas con el semistandard
+const $ = window.$;
+
+$('header').css('color', '#FF0000');
