7f80071e9d7807e1961699b10bdc1461c0a4ef5d
agusfl <agusflom@hotmail.com>
2022-09-19 14:13:55 +0000

    modify comments

--- 0x15-javascript-web_jquery/1-script.js
+++ 0x15-javascript-web_jquery/1-script.js
@@ -1,4 +1,5 @@
-// Indo JQuery: https://www.w3schools.com/jquery/jquery_syntax.asp
+// Write a JavaScript script that updates the text color of the <header> element to red (#FF0000)
+// Info JQuery: https://www.w3schools.com/jquery/jquery_syntax.asp
 // Defino $ para que no haya problemas con el semistandard
 const $ = window.$;
 
